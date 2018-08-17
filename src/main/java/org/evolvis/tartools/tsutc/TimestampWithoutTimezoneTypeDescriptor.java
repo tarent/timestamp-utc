ebcb32ce8ade153d852565e92ac6de5ee4bed5da
mirabilos <t.glaser@tarent.de>
2018-08-17 17:58:12 +0200

    mvn com.mycila:license-maven-plugin:3.0:format

--- src/main/java/org/evolvis/tartools/tsutc/TimestampWithoutTimezoneTypeDescriptor.java
+++ src/main/java/org/evolvis/tartools/tsutc/TimestampWithoutTimezoneTypeDescriptor.java
@@ -1,5 +1,30 @@
 package org.evolvis.tartools.tsutc;
 
+/*-
+ * tsutc (TimestampWithoutTimezone) is Copyright
+ *  © 2016, 2018 mirabilos (t.glaser@tarent.de)
+ * Licensor is tarent solutions GmbH, http://www.tarent.de/
+ *
+ * TimestampWithoutTimezoneTypeDescriptor is a derivative work of
+ * Hibernate, Relational Persistence for Idiomatic Java, 5.1.0.FINAL,
+ * Copyright © 2010, 2011, 2012, 2015, 2016 Red Hat, Inc. (under CLA)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License (as file COPYING in the META-INF directory) along with
+ * this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 import org.hibernate.type.descriptor.ValueBinder;
 import org.hibernate.type.descriptor.ValueExtractor;
 import org.hibernate.type.descriptor.WrapperOptions;
